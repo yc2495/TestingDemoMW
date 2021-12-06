17eabf6e3fa7578099e822497a05740278b56340
Raj <ychandra@student.bridgew.edu>
2021-12-06 15:14:59 -0500

    Testing completed

--- src/main/java/BasicGeom.java
+++ src/main/java/BasicGeom.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.Scanner;
 
 public class BasicGeom {
     public static double calcDistance(Point p1, Point p2){
@@ -8,5 +9,21 @@ public class BasicGeom {
         var intermediate = xchange*xchange + ychange*ychange;
         distance = Math.sqrt(intermediate);
         return distance;
+    }
+
+    public static void main(String[] args){
+        Scanner inputReader = new Scanner(System.in);
+        System.out.println("what is the x location of the first point");
+        var p1 = new Point();
+        p1.x = inputReader.nextInt();
+        System.out.println("what is the y location of the second point");
+        var p2 = new Point();
+        p2.x = inputReader.nextInt();
+        System.out.println("What is the y location of the second point");
+        p2.y = inputReader.nextInt();
+        System.out.println("p1 is: "+p1+ " and p2 is: "+p2);
+        System.out.println("The distance between your points is "+calcDistance(p1, p2));
+
+
     }
 }
